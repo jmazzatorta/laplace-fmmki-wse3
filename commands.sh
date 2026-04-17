e4de8540070e8cc7a837b668f6918ab8d3f7bc98
j.mazzatorta <j.mazzatorta@gmail.com>
2026-04-17 11:46:07 +0200

    [FIX] syntax fixes

--- commands.sh
+++ commands.sh
@@ -3,9 +3,6 @@ set -e
 cslc --arch=wse3 ./layout.csl \
   --fabric-dims=512,512 \
   --fabric-offsets=0,0 \
-  --params=cells_per_side:64 \
-  --params=BITS:6 \
-  --params=prob_range_u16:512 \
   --params=max_bodies_pe:2 \
   --params=MEMCPY_H2D_ID:0 \
   --params=MEMCPY_D2H_ID:1 \
